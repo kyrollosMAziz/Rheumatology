e7e5a81e5655ca5b1ddde5052937935fa18697d4
kyrollosMAziz <80505582+kyrollosMAziz@users.noreply.github.com>
2025-03-21 09:52:55 +0200

    Waypoint

--- Assets/_APP/Scripts/GameManagers/WaypointManager/WaypointManager.cs
+++ Assets/_APP/Scripts/GameManagers/WaypointManager/WaypointManager.cs
@@ -1,12 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class WaypointManager : GenericSingleton<WaypointManager>
 {
-    [SerializeField] List <WaypointSerialized> _waypoints;
-    public void InitiateWaypointMovement() 
+    [SerializeField] private List<WaypointSerialized> _waypoints;
+    [SerializeField] private float _fixedDistance;
+    [SerializeField] private float _smoothSpeed;
+
+    private LinkedList<WaypointSerialized> waypointsLinkedList;
+    private LinkedListNode<WaypointSerialized> waypointsNode;
+
+    private void Awake()
     {
-        
+        waypointsLinkedList = new LinkedList<WaypointSerialized>(_waypoints);
+        waypointsNode = waypointsLinkedList.First;
     }
+    public void InitiateWaypointMovement(GameSequencePhase _sequencePhase, Transform objectToMove)
+    {
+        WaypointSerialized _waypointSerialized = _waypoints.FirstOrDefault<WaypointSerialized>(w => w.sequencePhase == _sequencePhase);
+        if (_waypointSerialized == null)
+        {
+            return;
+        }
+        StartCoroutine(StartMoving());
+        IEnumerator StartMoving()
+        {
+            Transform target = waypointsNode.Value.waypoints.First<Transform>();
+            while (Vector3.Distance(objectToMove.position, target.position) > _fixedDistance)
+            {
+                objectToMove.Translate(target.position);
+                Vector3 direction = target.position - objectToMove.transform.position;
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                objectToMove.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _smoothSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+            if (CheckWaypointReached()) 
+            {
+                StartCoroutine(StartMoving());
+            }
+        }
+    }
+    private bool CheckWaypointReached()
+    {
+        waypointsNode = waypointsNode.Next;
+        return waypointsNode == null;
+    }
+
 }
--- Assets/_App/Scripts/GameManagers/SceneManager.cs
+++ Assets/_App/Scripts/GameManagers/SceneManager.cs
@@ -7,14 +7,6 @@ public class SceneManager : GenericSingleton<SceneManager>
     public Queue<GameSequence> GameEventsQueue;
     public List<GameSequence> GameEvents;
     public List<IQuestionAnswered> QuestionsAnswered;
-    public enum GameSequencePhase
-    {
-        phase0 = 0, phase1 = 1, phase2 = 2,
-        phase3 = 3, phase4 = 4, phase5 = 5, 
-        phase6 = 6, phase7 = 7, phase8 = 8,
-        phase9 = 9
-    };
-
     private bool isCursorActive;
     public bool IsCursorActive
     {
